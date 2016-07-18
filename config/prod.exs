c2ce373d909957af58e132e5806d170233a12391
Paul Daigle <j.paul.daigle@gmail.com>
2016-07-17 21:13:22 -0400

    add postgres to heroku

--- config/prod.exs
+++ config/prod.exs
@@ -1,1 +1,6 @@
 use Mix.Config
+config :snapshots, Snapshots.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL")
+  extensions: [{Postgrex.Extensions.JSON, library: Poison}]
+
