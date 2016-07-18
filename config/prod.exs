6b40474b53c2a05fd7d5bdaa8a961303d2be4bd5
Paul Daigle <j.paul.daigle@gmail.com>
2016-07-17 21:25:27 -0400

    Fix typo in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 config :snapshots, Snapshots.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
   extensions: [{Postgrex.Extensions.JSON, library: Poison}]
 
